6221b194d466a2555b98599384abdcc0b1a604fd
shaheersystems <shaheer.imran1429@gmail.com>
2023-09-05 19:19:42 +0500

    build error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Wrap from "./components/Wrap";
 import HeroSection from "./components/HeroSection";
